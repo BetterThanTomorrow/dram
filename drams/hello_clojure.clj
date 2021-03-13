76a1a9198901c14c67b8fe54ddfd21030bc268ec
Peter Strömberg <pez@pezius.com>
2021-03-13 17:46:45 +0100

    Ratio 1/18

--- drams/hello_clojure.clj
+++ drams/hello_clojure.clj
@@ -131,7 +131,7 @@
   -1.8      ; floating point
   0.18e2    ; exponent
   18.0M     ; big decimal
-  108/20    ; ratio
+  18/324    ; ratio
   18N       ; big integer
   0x12      ; hex
   022       ; octal
