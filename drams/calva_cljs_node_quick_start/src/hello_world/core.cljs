1423f9afd7eda3c81662372cf888243af01766d3
Peter Strömberg <pez@pezius.com>
2021-05-26 08:50:24 +0200

    Add a richer Rich Comment example

--- drams/calva_cljs_node_quick_start/src/hello_world/core.cljs
+++ drams/calva_cljs_node_quick_start/src/hello_world/core.cljs
@@ -1,15 +1,36 @@
 (ns hello-world.core)
 
-(println "Hello ClojureScript World!")
+;; Evaluate these forms by placing the cursor in them
+;; and press Alt+Enter.
+
+(defn hello [s]
+  (println (str "Hello " s "!")))
+
+(hello "ClojureScript World")
 
 (comment
-  ;; Evaluate these forms by placing the cursor in them
-  ;; and press Alt+Enter.
-  
+  ;; This is a Rich Comment block:
+  ;;   https://youtu.be/Qx0-pViyIDU?t=1229
+  ;; Evaluate the forms in it the same way as the ones above.
+
   (inc 1)
-  
   (map inc [1 2 3])
-  
-  ;; See also the Calva command:
-  ;; *Fire up the Getting Started REPL*
-  )
+
+  (defn fizz-buzz [n]
+    (cond (zero? (mod n 15)) "FizzBuzz"
+          (zero? (mod n 5))  "Buzz"
+          (zero? (mod n 3))  "Fizz"
+          :else              n))
+  ;; Evalute these steps with Ctrl+Alt+Enter after each line.
+  (->> (range 1 101)
+       (map fizz-buzz)
+       (filter number?)
+       (map inc)
+       (map fizz-buzz))
+
+  ;; Please edit the code in this block and see
+  ;; what happens when you evaluate.
+)
+
+;; Learn about the Calva REPL and about Clojure with
+;; the command: *Fire up the Getting Started REPL*.
